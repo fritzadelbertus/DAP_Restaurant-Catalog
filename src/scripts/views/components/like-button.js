15aea65320fbc1ba7bddd7ad91255ffb007590df
Fritz <furitsucode@gmail.com>
2023-02-28 20:35:51 +0700

    upload page source

--- src/scripts/views/components/like-button.js
+++
@@ -1,8 +0,0 @@
-class LikeButton extends HTMLElement {
-  connectedCallback() {
-    this.id = 'likeButton';
-    this.classList.add('touchable');
-  }
-}
-
-customElements.define('like-button', LikeButton);
